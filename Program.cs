e61cd3d3fc3a5b811589276e8e92ef49ff68c54b
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-01-02 22:22:12 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ascii_exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int asciiValue = int.Parse(Console.ReadLine());
+            char letter = (char)asciiValue;
+
+            Console.WriteLine(letter);
+        }
+    }
+}
